9e5ecdfa9eaf3e5f4ab6fe101113aa4137db8507
Shulin <574074818@qq.com>
2018-02-06 11:20:28 -0500

    adding xml

---
+++ ProblemApp/app/src/main/java/com/example/a57407/problemapp/Swith_choose_Activity.java
@@ -0,0 +1,13 @@
+package com.example.a57407.problemapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Swith_choose_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swith_choose_);
+    }
+}
